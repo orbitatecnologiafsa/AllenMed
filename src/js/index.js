09706f10f28af4a94e0f09367ea1dee3c8133fc3
Vitel <vitelrfs@outlook.com>
2024-08-07 17:33:07 -0300

    finalzição tela login

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,53 @@ function showPassword(){
     }
 }
 
+//mobile
+
+document.getElementById("btn-login2").addEventListener("click", function(event) {
+    const leftContainer = document.getElementById("left");
+    const rightContainer = document.getElementById("right");
+    const topBar = document.getElementsByClassName('top-bar')[0];
+    const bottomBar = document.getElementsByClassName('bottom-bar')[0];
+
+    topBar.classList.add('hidden');
+    bottomBar.classList.add('hidden');
+    leftContainer.classList.remove('hidden');
+    rightContainer.classList.add('hidden');
+
+    history.pushState({ page: "left" }, "left", "?left");
+    
+    setTimeout(() => {
+        leftContainer.style.display = "flex";
+        rightContainer.style.display = "none";
+    }, 500); 
+});
+
+window.addEventListener("popstate", function(event) {
+    console.log("Usuário clicou no botão voltar");
+    const leftContainer = document.getElementById("left");
+    const rightContainer = document.getElementById("right");
+
+    if (event.state && event.state.page === "left") {
+        leftContainer.classList.remove('hidden');
+        rightContainer.classList.add('hidden');
+
+        setTimeout(() => {
+            leftContainer.style.display = "flex";
+            rightContainer.style.display = "none";
+        }, 500); 
+    } else {
+        leftContainer.classList.add('hidden');
+        rightContainer.classList.remove('hidden');
+
+        setTimeout(() => {
+            leftContainer.style.display = "none";
+            rightContainer.style.display = "flex";
+        }, 500); 
+    }
+});
+
+
+//login
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
     login();
