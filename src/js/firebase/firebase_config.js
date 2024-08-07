ad9eb6a7859893393bc48fbf146fe445e88b5408
Vitel <vitelrfs@outlook.com>
2024-08-07 16:34:44 -0300

    Tela inicial login

---
+++ src/js/firebase/firebase_config.js
@@ -0,0 +1,15 @@
+import { } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js';
+import { } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-auth-compat.js';
+import { } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore-compat.js';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBBL0aPGEbZVJ5zqMQtrtnp6oOCDX80-UY",
+    authDomain: "allenmed.firebaseapp.com",
+    projectId: "allenmed",
+    storageBucket: "allenmed.appspot.com",
+    messagingSenderId: "389027727556",
+    appId: "1:389027727556:web:6b95e6f991bfb483802191",
+    measurementId: "G-1RCCQ3RY4L"
+  };
+  firebase.initializeApp(firebaseConfig);
---
+++ src/js/verificar_user.js
@@ -0,0 +1,15 @@
+import { } from './firebase/firebase_config.js';
+
+function verificarLogin(){
+       
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log("Usuário está autenticado.");
+        //window.location.href = "./src/pages/main_screen.html";
+      } else {
+        console.log("Usuário não está autenticado.");
+      }
+  });
+}
+
+verificarLogin();
